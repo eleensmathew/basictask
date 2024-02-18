bebf7e15accb802463430edc0f5563276b6f46cb
eleensmathew <eleensmathew@gmail.com>
2024-02-18 13:11:40 +0530

    made script better

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,5 @@
 source env/bin/activate
 git pull
-deactivate
-sudo systemctl restart gunicorn
+cd /home/eleensmathew/news/mysite
+sudo pkill gunicorn
+/home/eleensmathew/news/env/bin/gunicorn --access-logfile - --workers 3 --bind unix:/home/eleensmathew/gunicorn.sock mysite.wsgi:application --log-file gunicorn.log &
